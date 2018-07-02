2fe2c9916a9039f11137e5e3399e0b313a68846f
Karo Rönty <39708710+KaroRonty@users.noreply.github.com>
2018-07-02 21:45:39 +0300

    Update valuationpercentiles.r

--- valuationpercentiles.r
+++ valuationpercentiles.r
@@ -1,43 +1,9 @@
-library(httr) # downloading the xls(x) files
-library(readxl) # reading xls(x) files
 library(dplyr) # data formatting
 library(ggplot2) # plotting
 library(reshape2) # melting data for plotting
 
-# Get CAPE, P/E and P/D data from Shiller
-GET("http://www.econ.yale.edu/~shiller/data/ie_data.xls", write_disk(temp <- tempfile(fileext = ".xls")))
-shillerdata <- read_xls(temp, sheet = 3, skip = 7)
-
-# Format the years and months correctly
-corrected_dates <- expand.grid(1:12, 1871:2018)
-last_month <- length(grep("2018", shillerdata$Date))
-months_to_be_cut_off <- 12 - last_month
-corrected_dates <- head(corrected_dates, nrow(corrected_dates) - months_to_be_cut_off)
-# Add leading zeros
-corrected_dates$Var1 <- sprintf("%02d", as.numeric(corrected_dates$Var1))
-dates <- as.data.frame(paste(corrected_dates$Var2, corrected_dates$Var1, sep = "-"))
-names(dates) <- "dates"
-
-# Remove possible excess rows & add corrected dates back
-shillerdata <- head(shillerdata, nrow(dates))
-shillerdata <- cbind(dates, shillerdata)
-shillerdata$Date <- NULL
-
-# Get P/B data from Goyal
-GET("http://www.hec.unil.ch/agoyal/docs/PredictorData2017.xlsx", write_disk(temp <- tempfile(fileext = ".xls")))
-goyaldata <- read_xlsx(temp, sheet = 1)
-goyaldata <- select(goyaldata, c("yyyymm", "b/m"))
-
-# Make dates into same format as above and prepare names for joining
-goyaldata$yyyymm <- paste(substr(goyaldata$yyyymm, 1, 4), substr(goyaldata$yyyymm, 5, 6), sep = "-")
-names(goyaldata) <- c("dates", "bm")
-
-full_data <- full_join(shillerdata, goyaldata, by = "dates")
-
-# Replace written NAs with real NAs
-full_data$bm[full_data$bm == "NaN"] <- NA
-full_data$CAPE[full_data$CAPE == "NA"] <- NA
-full_data$CAPE <- as.numeric(full_data$CAPE)
+# Retrieve data from Shiller & Goyal
+source("shiller_goyal_data_retriever.r")
 
 # Calculate the needed variables
 full_data <- full_data %>% mutate(
