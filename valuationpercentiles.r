1665e578b4d1b2b47952c65108efaf14b8a38264
KaroRonty <karo.ronty@gmail.com>
2019-06-12 18:37:35 +0300

    Switched to dplyr & tidyr

--- valuationpercentiles.r
+++ valuationpercentiles.r
@@ -1,28 +1,36 @@
 library(dplyr) # data formatting
 library(ggplot2) # plotting
-library(reshape2) # melting data for plotting
+library(tidyr) # gathering
 
 # Retrieve data from Shiller & Goyal
 source("https://raw.githubusercontent.com/KaroRonty/ShillerGoyalDataRetriever/master/ShillerGoyalDataRetriever.r")
 
 # Calculate the needed variables
 full_data <- full_data %>% mutate(
-  "PE" = P/E,
-  "PB" = 1/as.numeric(bm),
-  "PD" = P/D
+  "PE" = P / E,
+  "PB" = 1 / as.numeric(bm),
+  "PD" = P / D
 )
 
 # Make a data frame containing only the new valuation variables
 ranked <- as.data.frame(cbind(full_data$dates,
-                 percent_rank(full_data$CAPE),
-                 percent_rank(full_data$PE),
-                 percent_rank(full_data$PB),
-                 percent_rank(full_data$PD)))
+                              percent_rank(full_data$CAPE),
+                              percent_rank(full_data$PE),
+                              percent_rank(full_data$PB),
+                              percent_rank(full_data$PD)))
+
 colnames(ranked) <- c("dates", "CAPE", "PE", "PB", "PD")
 
-melted <- melt(ranked, id.vars = "dates")
-melted$value <- as.numeric(melted$value)
-melted <- as.data.frame(melted)
+# Gather for plotting
+to_plot <- gather(ranked, variable, value, -dates)
 
-ggplot(data = melted, aes(x = as.Date(melted$dates, "%Y-%M"),y = value, color = variable)) +
-  facet_wrap(~variable) + geom_line(size = 1.1) + labs(x = "Date", y = "Percentile of valuation")
+# Convert to numeric and tibble
+to_plot <- to_plot %>%
+  mutate(value = as.numeric(value)) %>%
+  as_tibble()
+
+ggplot(data = to_plot, aes(x = as.Date(to_plot$dates, "%Y-%M"),
+                           y = value, color = variable)) +
+  facet_wrap(~variable) +
+  geom_line(size = 1.1) +
+  labs(x = "Date", y = "Percentile of valuation")
